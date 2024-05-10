c7c9ce1bd81284240ec268d55c7e454c0ef63b77
Nics-IT2R2 <160290934+Nics-IT2R2@users.noreply.github.com>
2024-05-10 23:20:23 +0800

    footer-update

--- WST-React/src/components/Footer.jsx
+++ WST-React/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export class Footer extends Component {
   render() {
     return (
-        <footer className="bg-gray-800 text-white py-4">
+        <footer className="bg-blue-900 text-white py-4">
             <div className="container mx-auto px-4">
-            <p>&copy; 2024 My Footer</p>
+            <p>&copy; 2024 My Footer | Nicole K. Camara | IT 2R2</p>
             </div>
         </footer>
     )
